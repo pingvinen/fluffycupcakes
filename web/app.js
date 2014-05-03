5ccd2ad54acb60fddec88698679c1ffd6636120c
Patrick <pingvinen@isflagen.dk>
2014-05-03 23:27:25 +0200

    Found a way to get nodejs to run automatically on "up" (possibly also boot) + whenever files change

--- web/app.js
+++ web/app.js
@@ -1,8 +1,16 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-	console.log('incoming request');
+	console.log('incoming request: '+req.method+' '+req.url);
+	console.log(req.headers);
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Fluffy Cupcakes welcomes you to the island\n');
+
+    var x = new Date();
+    var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+    var time = x.getHours()+':'+x.getMinutes()+':'+x.getSeconds();
+
+    res.end('Fluffy Cupcakes welcomes you to the island on this lovely '+ days[x.getDay()] +' at '+ time +'\n');
 }).listen(3000, "127.0.0.1");
+
+
 console.log('Server running at http://127.0.0.1:3000/');
